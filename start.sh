50d82fb25889b14de05c266435d1258f36894095
pita khan <khanpitakj@gmail.com>
2025-12-17 18:16:07 +0100

    uouou

--- start.sh
+++ start.sh
@@ -1,26 +1,14 @@
 #!/bin/sh
 set -eux
 
+export ASPNETCORE_DETAILEDERRORS=true
+export DOTNET_EnableDiagnostics=1
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+
 echo "PORT=${PORT:-<empty>}"
 echo "PWD=$(pwd)"
-echo "USER=$(id -un || true)"
 
-echo "LS ROOT:"
-ls -la /
-
-echo "LS current:"
 ls -la
-
-echo "LS out (if vorhanden):"
 ls -la out || true
-
-dotnet --info || true
-
-if [ ! -f ./out/PitaRadiowebseite.dll ]; then
-  echo "ERROR: ./out/PitaRadiowebseite.dll fehlt."
-  echo "Stelle sicher, dass 'dotnet publish -c Release -o out' im Build ausgeführt wurde."
-  ls -la ./out || true
-  exit 1
-fi
 
 exec dotnet ./out/PitaRadiowebseite.dll
